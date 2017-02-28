4cc6734ed5885b82650e1189eeb795288f55f7a5
Anadi Misra <anadi.msr@gmail.com>
2017-02-28 13:44:37 +0530

    added files for VM as slaves. and generated vms

---
+++ slave-templates/create-slave.rb
@@ -0,0 +1,28 @@
+require 'rubygems'
+require 'erb'
+
+# set up some variables that we want to replace in the template
+$name = ARGV[0]
+$descr = ARGV[1]
+$executors = ARGV[2]
+$host = ARGV[3]
+$port = ARGV[4]
+$label = ARGV[5]
+JENKINS_HOME="/Users/anadi/Code/meetup/cgi-devops/jenkins/home"
+
+# method update_tokens takes template_file, expecting globals
+# to be set, and will return an updated string with tokens replaced.
+# you can either save to a new file, or output to the user some
+# other way.
+def update_tokens(template_file)
+ template = ""
+ open(template_file) {|f|
+   template = f.to_a.join
+ }
+ updated = ERB.new(template, 0, "%<>").result
+
+ return updated
+end
+
+new_xml=update_tokens("/Users/anadi/Code/meetup/cgi-devops/slave-templates/slave-node-template.xml")
+File.open("slave-#{ARGV[0]}-#{ARGV[5]}.xml", 'w') {|f| f.write(new_xml) }
---
+++ slave-templates/create-vagrant-vm.rb
@@ -0,0 +1,31 @@
+require 'rubygems'
+require 'erb'
+require 'fileutils'
+
+# set up some variables that we want to replace in the template
+$name = ARGV[0]
+$sshport = ARGV[1]
+$webport = ARGV[2]
+$tcport = ARGV[3]
+$memory = ARGV[4]
+
+JENKINS_HOME="/Users/anadi/Code/meetup/cgi-devops/jenkins/home"
+VAGRANT_ROOT="/Users/anadi/Code/meetup/cgi-devops/vagrant-vms"
+
+# method update_tokens takes template_file, expecting globals
+# to be set, and will return an updated string with tokens replaced.
+# you can either save to a new file, or output to the user some
+# other way.
+def update_tokens(template_file)
+ template = ""
+ open(template_file) {|f|
+   template = f.to_a.join
+ }
+ updated = ERB.new(template, 0, "%<>").result
+
+ return updated
+end
+
+new_vagrant=update_tokens("/Users/anadi/Code/meetup/cgi-devops/slave-templates/vagrant-template")
+Dir.mkdir "#{VAGRANT_ROOT}/#{ARGV[0]}"
+File.open("#{VAGRANT_ROOT}/#{ARGV[0]}/Vagrantfile", 'w') {|f| f.write(new_vagrant) }
